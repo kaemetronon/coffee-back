464517b1cffe1fc90a8fad25fbe1bd5ef5591173
igor <kametronon@mail.ru>
2021-02-24 01:44:03 +0300

    немного аннотаций для свагера

--- src/main/java/ru/coffee/nostresso/controller/Controller.java
+++ src/main/java/ru/coffee/nostresso/controller/Controller.java
@@ -1,5 +1,7 @@
 package ru.coffee.nostresso.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.coffee.nostresso.model.Shop;
@@ -16,6 +18,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/")
 @AllArgsConstructor
+@Api(value = "main controller")
 public class Controller {
 
     private final ShopRepo shopRepo;
@@ -24,16 +27,19 @@ public class Controller {
     private final HistoryRepo historyRepo;
 
     @GetMapping("/coffees")
+    @ApiOperation(value = "get all coffees", response = Shop[].class)
     public Iterable<Shop> getAllCoffeeshops() {
         return shopRepo.findAll();
     }
 
     @GetMapping("/shop")
+    @ApiOperation(value = "get items by coffeeshop", response = Item[].class)
     public Iterable<Item> getItemsFromCoffeeShop(@RequestParam UUID shopId) {
         return itemRepo.findByShopId(shopId);
     }
 
     @GetMapping("/history")
+    @ApiOperation(value = "get all history", response = OrderHistory[].class)
     public Optional<OrderHistory> getHistory(@RequestParam UUID userId) {
         return historyRepo.findByUserId(userId);
     }
