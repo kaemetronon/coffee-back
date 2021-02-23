18c21be7a3287fbf76cde1dbe03dab30138ba1d7
igor <kametronon@mail.ru>
2021-02-24 01:22:55 +0300

    swagger added

--- src/main/java/ru/coffee/nostresso/NoStressoApplication.java
+++ src/main/java/ru/coffee/nostresso/NoStressoApplication.java
@@ -4,17 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.coffee.nostresso.model.Address;
-import ru.coffee.nostresso.model.Item;
-import ru.coffee.nostresso.model.Review;
-import ru.coffee.nostresso.model.Shop;
 import ru.coffee.nostresso.repo.ShopRepo;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class NoStressoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
--- src/main/java/ru/coffee/nostresso/model/Shop.java
+++ src/main/java/ru/coffee/nostresso/model/Shop.java
@@ -3,7 +3,6 @@ package ru.coffee.nostresso.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
