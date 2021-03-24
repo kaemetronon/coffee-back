f9f92f89b52bdb87dbdc4a945a864bf23e84d1c9
i.pomerantsev <i.pomerantsev@telda.ru>
2021-03-24 17:28:16 +0300

    теперь запускается через терминал

--- src/main/java/ru/coffee/nostresso/NoStressoApplication.java
+++ src/main/java/ru/coffee/nostresso/NoStressoApplication.java
@@ -1,24 +1,14 @@
 package ru.coffee.nostresso;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.coffee.nostresso.model.mapper.ReviewMapper;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@EnableSwagger2
-public class NoStressoApplication implements CommandLineRunner {
+public class NoStressoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NoStressoApplication.class, args);
     }
 
-
-    @Override
-    public void run(String... args) {
-
-    }
 }
--- src/main/java/ru/coffee/nostresso/controller/common/ItemController.java
+++ src/main/java/ru/coffee/nostresso/controller/common/ItemController.java
@@ -21,5 +21,4 @@ public class ItemController {
     public List<Item> getItemByShopId(@RequestParam Long shopId) {
         return itemService.getItemsByShop(shopId);
     }
-
 }
