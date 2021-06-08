b86bb59069878422482d984a233a3c0c444541fc
igor <kametronon@mail.ru>
2021-06-09 01:02:05 +0300

    add @CrossOrigin in controller

--- src/main/java/ru/coffee/nostresso/controller/LoginController.java
+++ src/main/java/ru/coffee/nostresso/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import ru.coffee.nostresso.model.dto.TokenResponse;
 import ru.coffee.nostresso.security.JwtService;
 import ru.coffee.nostresso.security.VkParamsParseService;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 public class LoginController {
