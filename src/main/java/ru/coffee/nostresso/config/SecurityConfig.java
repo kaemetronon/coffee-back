1998e24e98052d36be335c1db4b8c5f97ae5501e
Igor Pomerantsev <igor.pomerantsev@umbrellait.com>
2021-06-09 12:30:21 +0300

    add swagger to whitelist

--- src/main/java/ru/coffee/nostresso/config/SecurityConfig.java
+++ src/main/java/ru/coffee/nostresso/config/SecurityConfig.java
@@ -23,13 +23,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     private final MyUserDetailsService userDetailsService;
     private final JwtFilter jwtFilter;
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth").permitAll()
-                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
